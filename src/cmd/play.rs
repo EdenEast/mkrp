6742a3de0d604391252cf92d56b8e885654849da
EdenEast <edenofest@gmail.com>
2023-07-26 13:48:55 -0400

    feat(ui): add basic progressbars
    In the future it would be great for this to be a progress of time
    instead of events. That is something to be worked on.

--- src/cmd/play.rs
+++ src/cmd/play.rs
@@ -1,10 +1,12 @@
 use std::{
+    hint::spin_loop,
     sync::mpsc::channel,
     thread,
     time::{Duration, Instant},
 };
 
 use crossbeam_channel::{bounded, tick, unbounded, Receiver};
+use indicatif::{HumanDuration, MultiProgress, ProgressBar, ProgressStyle};
 use rdev::{listen, simulate, EventType};
 
 use crate::{
@@ -30,11 +32,30 @@ impl Run for Play {
             None => KeyState::with_pressed(&[Key::Escape]),
         };
 
-        let delay = self.delay.map(|s| Duration::from_millis(s));
         let total_iterations = self.iterations.unwrap_or(1);
+        let delay = self
+            .delay
+            .map(|s| Duration::from_millis(s))
+            .unwrap_or(Duration::ZERO);
+
+        let total_time = (session.total_time * total_iterations) + (delay * (total_iterations - 1));
+
+        // Dump information on play
+        println!("Session run time: {}", HumanDuration(session.total_time));
+        println!("Total run time:   {}", HumanDuration(total_time));
+
+        let style =
+            ProgressStyle::with_template("{prefix} {wide_bar} ({pos:^5}/{len:^5}) {percent:>3}%")
+                .unwrap();
+        let mp = MultiProgress::new();
+        let tpb = mp.add(ProgressBar::new(total_iterations as u64));
+        let spb = mp.add(ProgressBar::new(session.events.len() as u64));
+        tpb.set_prefix("Total  ");
+        spb.set_prefix("Session");
+        tpb.set_style(style.clone());
+        spb.set_style(style);
 
         let (tt, rt) = unbounded();
-        // let (tx, rx) = unbounded();
 
         let tt_input = tt.clone();
         let listener = thread::spawn(move || {
@@ -56,28 +77,67 @@ impl Run for Play {
             .expect("Could not listen");
         });
 
-        let (tt_exec, rt_exec) = (tt.clone(), rt.clone());
-        let executor = thread::spawn(move || {
-            let (tt, rt) = (tt_exec, rt_exec);
-            println!("executor started");
-            for i in 0..total_iterations {
-                println!("Iterations {}", i);
-                for event in &session.events {
-                    if let Ok(msg) = rt.try_recv() {
-                        println!("term msg recv in executor thread");
-                        tt.send(true)
-                            .unwrap_or_else(|_| println!("Could not send terminate event"));
-                        return;
-                    }
-                    spin_sleep::sleep(event.delay);
-                    simulate(&event.event).expect(&format!("failed to simulate {:#?}", event));
+        let mut has_terminated = false;
+        tpb.tick();
+        spb.tick();
+        for i in 0..total_iterations {
+            spb.set_position(0);
+            for event in &session.events {
+                if let Ok(msg) = rt.try_recv() {
+                    println!("term msg recv");
+                    has_terminated = true;
+                    break;
                 }
+
+                spin_sleep::sleep(event.delay);
+                simulate(&event.event).expect(&format!("failed to simulate {:#?}", event));
+
+                spb.inc(1);
             }
 
-            tt.send(true)
-                .expect("failed to send terminate signal from exec thread");
-            println!("executor thread finished");
-        });
+            if has_terminated {
+                break;
+            }
+
+            // No need if we are at the final iteration
+            if i < total_iterations - 1 {
+                spin_sleep::sleep(delay);
+            }
+
+            tpb.inc(1);
+        }
+
+        tpb.finish_and_clear();
+        spb.finish_and_clear();
+        mp.clear();
+
+        // let (tt_exec, rt_exec) = (tt.clone(), rt.clone());
+        // let executor = thread::spawn(move || {
+        //     let (tt, rt) = (tt_exec, rt_exec);
+        //     println!("executor started");
+        //     for i in 0..total_iterations {
+        //         // println!("Iterations {}", i);
+        //         // for event in &session.events {
+        //             if let Ok(msg) = rt.try_recv() {
+        //                 println!("term msg recv in executor thread");
+        //                 tt.send(true)
+        //                     .unwrap_or_else(|_| println!("Could not send terminate event"));
+        //                 return;
+        //             }
+        //             spin_sleep::sleep(event.delay);
+        //             simulate(&event.event).expect(&format!("failed to simulate {:#?}", event));
+        //         }
+        //
+        //         // No need if we are at the final iteration
+        //         if i < total_iterations - 1 {
+        //             spin_sleep::sleep(delay);
+        //         }
+        //     }
+        //
+        //     tt.send(true)
+        //         .expect("failed to send terminate signal from exec thread");
+        //     println!("executor thread finished");
+        // });
 
         // let (tt_ui, rt_ui) = (tt.clone(), rt.clone());
         // let ui = thread::spawn(move || {
@@ -102,7 +162,7 @@ impl Run for Play {
         // });
         // ui.join();
 
-        executor.join();
+        // executor.join();
 
         Ok(())
     }
