49e71a68cd920b2168b7b35e0a1a1070e8f9db59
EdenEast <edenofest@gmail.com>
2023-07-26 23:47:50 -0400

    feat(play): add eta to progressbar

--- src/cmd/play.rs
+++ src/cmd/play.rs
@@ -49,6 +49,7 @@ impl Run for Play {
         let total_duration =
             (session.total_time * total_iterations) + (delay * (total_iterations - 1));
         let total_formatted_duration = FormattedDuration(total_duration);
+        let session_duration = session.total_time;
         let session_formatted_dutation = FormattedDuration(session.total_time);
 
         // Terminate channel
@@ -154,19 +155,27 @@ impl Run for Play {
                 tpb.inc(1);
                 spb.inc(1);
 
+                let cur_total_duration = now.saturating_duration_since(initial_start);
+                let cur_total_eta = total_duration - cur_total_duration;
+
+                let cur_session_duration = now.saturating_duration_since(session_start);
+                let cur_session_eta = session_duration - cur_session_duration;
+
                 tpb.set_message(format!(
-                    "({:>5}/{:>5}) {} / {}",
+                    "({:>5}/{:>5}) [{} / {}] ({})",
                     current_total,
                     total_iterations,
-                    FormattedDuration(now.saturating_duration_since(initial_start)),
-                    total_formatted_duration
+                    FormattedDuration(cur_total_duration),
+                    total_formatted_duration,
+                    FormattedDuration(cur_total_eta)
                 ));
                 spb.set_message(format!(
-                    "({:>5}/{:>5}) {} / {}",
+                    "({:>5}/{:>5}) [{} / {}] ({})",
                     current_event,
                     total_session,
                     FormattedDuration(now.saturating_duration_since(session_start)),
-                    session_formatted_dutation
+                    session_formatted_dutation,
+                    FormattedDuration(cur_session_eta)
                 ));
             }
         }
@@ -176,93 +185,6 @@ impl Run for Play {
         mp.clear();
 
         println!("bye");
-
-        // let mut has_terminated = false;
-        // for i in 0..total_iterations {
-        //     spb.set_position(0);
-        //     for event in &session.events {
-        //         if let Ok(msg) = rt.try_recv() {
-        //             println!("term msg recv");
-        //             has_terminated = true;
-        //             break;
-        //         }
-        //
-        //         spin_sleep::sleep(event.delay);
-        //         simulate(&event.event).expect(&format!("failed to simulate {:#?}", event));
-        //
-        //         spb.inc(1);
-        //     }
-        //
-        //     if has_terminated {
-        //         break;
-        //     }
-        //
-        //     // No need if we are at the final iteration
-        //     if i < total_iterations - 1 {
-        //         spin_sleep::sleep(delay);
-        //     }
-        //
-        //     tpb.inc(1);
-        // }
-        //
-        // let ticker = tick(Duration::from_millis(500));
-        //
-        // tpb.finish_and_clear();
-        // spb.finish_and_clear();
-        // mp.clear();
-
-        // let (tt_exec, rt_exec) = (tt.clone(), rt.clone());
-        // let executor = thread::spawn(move || {
-        //     let (tt, rt) = (tt_exec, rt_exec);
-        //     println!("executor started");
-        //     for i in 0..total_iterations {
-        //         // println!("Iterations {}", i);
-        //         // for event in &session.events {
-        //             if let Ok(msg) = rt.try_recv() {
-        //                 println!("term msg recv in executor thread");
-        //                 tt.send(true)
-        //                     .unwrap_or_else(|_| println!("Could not send terminate event"));
-        //                 return;
-        //             }
-        //             spin_sleep::sleep(event.delay);
-        //             simulate(&event.event).expect(&format!("failed to simulate {:#?}", event));
-        //         }
-        //
-        //         // No need if we are at the final iteration
-        //         if i < total_iterations - 1 {
-        //             spin_sleep::sleep(delay);
-        //         }
-        //     }
-        //
-        //     tt.send(true)
-        //         .expect("failed to send terminate signal from exec thread");
-        //     println!("executor thread finished");
-        // });
-
-        // let (tt_ui, rt_ui) = (tt.clone(), rt.clone());
-        // let ui = thread::spawn(move || {
-        //     let (tt, rt) = (tt_ui, rt_ui);
-        //     println!("ui thread started");
-        //
-        //     let ticker: Receiver<Instant> = tick(std::time::Duration::from_millis(500));
-        //
-        //     loop {
-        //         crossbeam_channel::select! {
-        //             recv(rt) -> _ => {
-        //                 println!("term msg recv in ui thread");
-        //                 tt.send(true)
-        //                     .unwrap_or_else(|_| println!("Could not send terminate event"));
-        //                 return;
-        //             },
-        //             recv(ticker) -> _ => {
-        //
-        //             }
-        //         };
-        //     }
-        // });
-        // ui.join();
-
-        // executor.join();
 
         Ok(())
     }
